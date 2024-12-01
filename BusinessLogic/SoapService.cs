7b27c0c5ada0550cb2a3aef6d92b28192d76a222
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-01 14:39:44 -0500

    feat: :sparkles: Create the SoapService.cs
    This service helps to add two integer numbers

---
+++ BusinessLogic/SoapService.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+
+namespace CsSoapApi.BusinessLogic
+{
+    [ServiceContract]
+    public interface ISoapService
+    {
+        [OperationContract]
+        string Sum(int num1, int num2);
+    }
+    public class SoapService : ISoapService
+    {
+        public string Sum(int num1, int num2)
+        {
+            return $"Sum of {num1} plus {num2} is: {num1 + num2}";
+        }
+    }
+}
